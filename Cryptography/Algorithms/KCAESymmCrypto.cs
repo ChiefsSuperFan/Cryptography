a73e7cf846128e2d0adc3b2e74510d4a5d56502b
Anthony Stone <Anthony.Stone@formativelogic.com>
2019-07-21 16:18:44 -0400

    aes buildout

--- Cryptography/Algorithms/KCAESymmCrypto.cs
+++ Cryptography/Algorithms/KCAESymmCrypto.cs
@@ -10,7 +10,12 @@ namespace Cryptography.Algorithms
 {
     public static class KCAESymmCrypto
     {
-        private const string CONCAT = "|";
+        private const char CONCAT = '|';
+
+        private const int ITERATION_COUNT = 1111;
+        private const int KEY_SIZE = 256;
+        private const int IV_LENGTH = 16;
+   
 
         public static string EncryptFile(string inputFile, string outputFile)
         {
@@ -36,6 +41,7 @@ namespace Cryptography.Algorithms
 
                         using (var cryptoStream = new CryptoStream(destinationStream, cryptoTransform, CryptoStreamMode.Write))
                         {
+                         
                             sourceStream.CopyTo(cryptoStream);
                         }
 
@@ -56,8 +62,8 @@ namespace Cryptography.Algorithms
             //keyIV is the concatenated key and iv
             try
             {
-                string key = keyIV.Split('|').FirstOrDefault();
-                string iv = keyIV.Split('|').LastOrDefault();
+                string key = keyIV.Split(CONCAT).FirstOrDefault();
+                string iv = keyIV.Split(CONCAT).LastOrDefault();
 
                 byte[] aesKey = Convert.FromBase64String(key);
                 byte[] ivValue = Convert.FromBase64String(iv);
@@ -82,6 +88,104 @@ namespace Cryptography.Algorithms
 
 
 
+        }
+
+        public static string DecryptText(string encryptedText, string passwordSeed)
+        {
+            try
+            {
+                byte[] aesKey = GetAESKey(passwordSeed);
+
+                byte[] source = Convert.FromBase64String(encryptedText);
+                byte[] aesIV = source.Take(IV_LENGTH).ToArray();
+                byte[] cipherText = source.Skip(IV_LENGTH).ToArray();
+                string plainText = "";
+
+                using (AesCryptoServiceProvider aes = new AesCryptoServiceProvider())
+                {
+                    aes.Key = aesKey;
+                    aes.IV = aesIV;
+                    ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                    // Create the streams used for decryption.
+                    using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                    {
+                        using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                            {
+
+                                // Read the decrypted bytes from the decrypting stream
+                                // and place them in a string.
+                                plainText = srDecrypt.ReadToEnd();
+                            }
+                        }
+                    }
+
+                }
+                return plainText;
+
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        public static string EncryptText(string inputText, string passwordSeed)
+        {
+            //this encrypts text and uses a password seed to create the key, instead of 
+            //using the AES providor.  The IV is included in the encrypted text
+            //passwordSeed is random text that would be stored somewhere in a db
+            
+            try
+            {
+                byte[] encrypted;
+                byte[] aesKey = GetAESKey(passwordSeed);
+
+                using (AesCryptoServiceProvider aes = new AesCryptoServiceProvider())
+                {
+                    // Create a decryptor to perform the stream transform.
+                    aes.Key = aesKey;
+                    int ivLength = aes.IV.Length;
+                    // Create an encryptor to perform the stream transform.
+                    ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                    // Create the streams used for encryption.
+                    using (MemoryStream msEncrypt = new MemoryStream())
+                    {
+                        using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        {
+                            //destinationStream.Write(provider.IV, 0, provider.IV.Length);
+                            msEncrypt.Write(aes.IV, 0, aes.IV.Length);
+                            using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                            {
+                                //Write all data to the stream.
+                                swEncrypt.Write(inputText);
+                            }
+                            encrypted = msEncrypt.ToArray();
+                        }
+                    }
+
+                }
+                return Convert.ToBase64String(encrypted);
+
+            }catch
+            {
+                return "";
+            }
+        }
+
+        private static byte[] GetAESKey(string passwordSeed)
+        {
+            byte[] salt = { 0x0, 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6,
+                    0xF1, 0xF0, 0xEE, 0x21, 0x22, 0x45 };
+
+            using (Rfc2898DeriveBytes deriveBytes = new Rfc2898DeriveBytes(passwordSeed, salt, ITERATION_COUNT))
+            {
+                byte[] key = deriveBytes.GetBytes(KEY_SIZE / 8);
+                return key;
+            }
         }
     }
 }
